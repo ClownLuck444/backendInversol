3650949ee2ac62fee42644f1482bc85f71f0d01b
ClownLuck444 <natanaelmurod@gmail.com>
2025-06-02 19:04:19 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
+from urllib.parse import quote_plus
 
 class Config:
     SERVER = os.getenv("MYSQL_SERVER", "167.114.196.68")
     DATABASE = os.getenv("MYSQL_DB", "inv3rsol_prueba")
     USERNAME = os.getenv("MYSQL_USER", "inv3rsol_sa")
-    PASSWORD = os.getenv("MYSQL_PASSWORD", "Inversol123@")
+    PASSWORD = quote_plus(os.getenv("MYSQL_PASSWORD", "Inversol123@"))  # codifica @ correctamente
     PORT = os.getenv("MYSQL_PORT", "3306")
 
     SQLALCHEMY_DATABASE_URI = (
